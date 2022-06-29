24a52e570aff7c09294ea565a146e8db72746c4a
jtm2021 <jesson_chubz@yahoo.com.ph>
2022-06-29 05:22:32 +0000

    Add first testing

---
+++ blog/tests/test_urls.py
@@ -0,0 +1,9 @@
+from django.test import SimpleTestCase
+from blog.views import PostList
+from django.urls import reverse,resolve
+
+class TestUrls(SimpleTestCase):
+
+    def test_post_list_url(self):
+        url = reverse("home")
+        self.assertEquals("/", url)
