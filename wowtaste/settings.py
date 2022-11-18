8d526233836d17dfa7ca09240ce75710c3fef266
jtm2021 <jesson_chubz@yahoo.com.ph>
2022-11-18 19:44:09 +0000

    Migrate date from heroku to elephantsql and update settings.py

--- wowtaste/settings.py
+++ wowtaste/settings.py
@@ -36,6 +36,11 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['wowtaste2022.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
