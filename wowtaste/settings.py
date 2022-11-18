c05145f47c2f3d689cc77367867923eb1a3045ed
jtm2021 <jesson_chubz@yahoo.com.ph>
2022-11-18 22:00:57 +0000

    Edit settings.py

--- wowtaste/settings.py
+++ wowtaste/settings.py
@@ -39,8 +39,7 @@ ALLOWED_HOSTS = ['wowtaste2022.herokuapp.com', 'localhost']
 # Add Render.com URL to allowed hosts
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
-   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
